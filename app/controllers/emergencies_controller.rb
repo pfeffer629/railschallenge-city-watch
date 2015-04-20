f8ec8a6bd73e1591c1ae2817265078c29989f14b
Mark Pfeffer <pfeffer629@gmail.com>
2015-04-20 14:04:10 -0700

    PATCH /emergencies/:code changes severity levels

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -1,7 +1,4 @@
 class EmergenciesController < ApplicationController
-  def new
-  end
-
   def index
     @emergencies = Emergency.all
     if @emergencies.empty?
@@ -28,6 +25,14 @@ class EmergenciesController < ApplicationController
     else
       render json: {}, status: 404
     end
+  end
+
+  def update
+    @emergency = Emergency.find_by(code: params[:id])
+    @emergency.fire_severity = params['emergency']['fire_severity'].to_i
+    @emergency.police_severity = params['emergency']['police_severity'].to_i
+    @emergency.medical_severity = params['emergency']['medical_severity'].to_i
+    render json: { emergency: @emergency }
   end
 
   private
