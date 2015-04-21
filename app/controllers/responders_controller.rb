2dc66cb01e869c2d79c10c1b51eaced2248c9ab1
Mark Pfeffer <pfeffer629@gmail.com>
2015-04-21 12:51:55 -0700

    pulling from master

--- app/controllers/responders_controller.rb
+++ app/controllers/responders_controller.rb
@@ -1,19 +1,22 @@
 class RespondersController < ApplicationController
+  before_action :page_not_found, only: [:new, :edit, :destroy]
+  before_action :find_responder, only: [:show, :update]
+  rescue_from ActionController::UnpermittedParameters, with: :show_errors
+
   def new
-    page_not_found
   end
 
   def edit
-    page_not_found
   end
 
   def destroy
-    page_not_found
   end
 
   def index
     @responders = Responder.all
-    if @responders.empty?
+    if params
+      show_capacity
+    elsif @responders.empty?
       render json: { responders: [] }
     else
       render json: { responders: @responders }, status: 200
@@ -21,7 +24,7 @@ class RespondersController < ApplicationController
   end
 
   def create
-    @responder = Responder.new(responder_params)
+    @responder = Responder.new(create_responder_params)
     if @responder.save
       render json: { responder: @responder }, status: 201
     else
@@ -40,17 +43,56 @@ class RespondersController < ApplicationController
   end
 
   def update
-    @responder = Responder.find_by(name: params[:id])
-    @responder.on_duty = params['responder']['on_duty']
-    render json: { responder: @responder }
+    if @responder.update(update_responder_params)
+      render json: { responder: @responder }
+    else
+      @errors = @responder.errors.messages
+      render json: { message: @errors }
+    end
   end
 
   private
-    def responder_params
-      params.require(:responder).permit(:emergency_code, :type, :name, :capacity, :on_duty)
-    end
 
-    def page_not_found
-      render json: { message: 'page not found' }, status: 404
+  def create_responder_params
+    params.require(:responder).permit(:type, :name, :capacity)
+  end
+
+  def update_responder_params
+    params.require(:responder).permit(:on_duty)
+  end
+
+  def find_responder
+    @responder = Responder.find_by(name: params[:id])
+  end
+
+  def show_capacity
+    @capacity = { Fire: [0, 0, 0, 0],
+      Police: [0, 0, 0, 0],
+      Medical: [0, 0, 0, 0] }
+    @responders.each do |responder|
+      if responder.type == 'Fire'
+        @capacity[Fire][0] += 1
+        @capacity[Fire][1] += 1
+        if responder.on_duty
+          @capacity[Fire][2] += 1
+          @capacity[Fire][3] += 1
+        end
+      elsif responder.type == 'Police'
+        @capacity[Police][0] += 1
+        @capacity[Police][1] += 1
+        if responder.on_duty
+          @capacity[Police][2] += 1
+          @capacity[Police][3] += 1
+        end
+      else
+        @capacity[Medical][0] += 1
+        @capacity[Medical][1] += 1
+        if responder.on_duty
+          @capacity[Medical][2] += 1
+          @capacity[Medical][3] += 1
+        end
+      end
     end
+    render json: { capacity: @capacity }
+  end
 end
